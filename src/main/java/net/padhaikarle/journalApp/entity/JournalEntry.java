804f863df810ee870a7ad2228e68862d60bb41f4
Suhail Ansari <sansari@clari.com>
2025-08-09 19:58:44 +0530

    Implementing Lombok

--- src/main/java/net/padhaikarle/journalApp/entity/JournalEntry.java
+++ src/main/java/net/padhaikarle/journalApp/entity/JournalEntry.java
@@ -1,52 +1,21 @@
 package net.padhaikarle.journalApp.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
-import org.springframework.cglib.core.Local;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Getter
+@Setter
 public class JournalEntry {
     @Id
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
     private String author;
 }
